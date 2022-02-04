3c4a24b0ada3c23a89cd3ee03f4a1cf865bb8d79
PerrinGaetan <perringaetandu31@gmail.com>
2022-02-04 11:04:57 +0100

    Add some style and add README file

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -35,7 +35,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('search_filter_index');
+            return $this->redirectToRoute('application_index');
         }
 
         return $this->render('registration/register.html.twig', [
